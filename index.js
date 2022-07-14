b7f921dabdd6bce52ed43e58ad9402c7a57a461b
Hulff <ph2717693@Gmail.com>
2022-07-14 18:11:29 -0300

    updating routes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 
-app.get("/main" , (req,res)=>{
+
+app.get("/", (req,res)=>{
     res.send({data:"hi"})
 })
 
-app.listen(8080, ()=>{console.log("http://localhost:8080")})
+app.get("/main" , (req,res)=>{
+    res.send({data:"hi"})
+})
