3b5fe1c8e3997d70496a31b6a95d80ec8f5bd2b7
Hulff <ph2717693@Gmail.com>
2022-07-18 19:42:37 -0300

    Cors editing

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const app = express()
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 
 app.get("/", (req,res)=>{
     res.send({
