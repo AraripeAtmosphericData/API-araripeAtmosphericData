32b60831a9633f42c50d7b7a1bdbefba719b2c26
Hugo <91216522+Hulff@users.noreply.github.com>
2022-10-23 15:19:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/saveData/:identificador/:bateria/:temp/:pressao/:voc/:co2', (req, res
     let temperatura = req.params.temp
     let pressao = req.params.pressao
     let payload = {
-        altitude: (44331.5-(494.62*(pressao**0.190263))),
+        altitude: (44331.5-(4946.62*(pressao**0.190263))),
         co2: req.params.co2,
         voc: req.params.voc
     }
